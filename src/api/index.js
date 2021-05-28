7efa22d54612df1c8016d5757e861865e6173cf4
Keshav Jaiswal <keshav.jaiswal39@gmail.com>
2021-05-28 22:47:08 +0530

    Completed the api call to get the recoveries, deaths and active case data

--- src/api/index.js
+++ src/api/index.js
@@ -13,3 +13,13 @@ export const fetchData = async () => {
 
     }
 }
+
+export const fetchDailyData = async () => {
+    try{
+        const { data } = await axios.get(`$(url)/daily`);
+    }
+    catch (error)
+    {
+
+    }
+}
